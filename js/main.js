544e4e7ce384d639cfe52aac557f2301d7846fc1
Irwin Chen <irwin.t.chen@gmail.com>
2022-04-19 22:58:11 +0000

    completed event listener for recipe info button, server request for recipe in working condition, server response saved in new variable

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 var $form = document.querySelector('form');
 var $searchBar = document.querySelector('input');
 var $recipeContainer = document.querySelector('.recipe-container');
-var query;
+var query = null;
+var recipeId = null;
+var recipe = null;
 
 $form.addEventListener('submit', function (event) {
   event.preventDefault();
@@ -13,26 +15,20 @@ $form.addEventListener('submit', function (event) {
   }
   query = $searchBar.value;
   parseQuery();
-  $form.reset();
   serverRequest();
+  $form.reset();
+});
+
+$recipeContainer.addEventListener('click', function (event) {
+  if (event.target.matches('button')) {
+    recipeId = event.target.getAttribute('recipeid');
+  }
+  recipeRequest();
 });
 
 function parseQuery() {
   query = query.trim();
   query = query.replaceAll(' ', ',+');
-}
-
-function serverRequest() {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://api.spoonacular.com/recipes/findByIngredients' + '?ingredients=' + query + '&number=6&ranking=1&ignorePantry=true&apiKey=a264ae5f3ca9445d94cfeb08761ae88b');
-  xhr.responseType = 'json';
-  xhr.addEventListener('load', function () {
-    query = xhr.response;
-    for (var queryIndex = 0; queryIndex < query.length; queryIndex++) {
-      recipePreview(queryIndex);
-    }
-  });
-  xhr.send();
 }
 
 function recipePreview(index) {
@@ -53,15 +49,32 @@ function recipePreview(index) {
   var $recipeInfoButton = document.createElement('button');
   $recipeInfoButton.className = 'info-button text-white';
   $recipeInfoButton.textContent = 'READ MORE';
-  $recipeInfoButton.setAttribute('recipeId', query[index].id);
+  $recipeInfoButton.setAttribute('recipeid', query[index].id);
 
   $recipePreviewCard.append($recipeImage, $recipeName, $recipeInfoButton);
   $recipePreviewEntry.append($recipePreviewCard);
   $recipeContainer.append($recipePreviewEntry);
 }
 
-$recipeContainer.addEventListener('click', function (event) {
-  if (event.target.matches('button')) {
-    return null;
-  }
-});
+function serverRequest() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://api.spoonacular.com/recipes/findByIngredients' + '?ingredients=' + query + '&number=6&ranking=1&ignorePantry=true&apiKey=a264ae5f3ca9445d94cfeb08761ae88b');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    query = xhr.response;
+    for (var queryIndex = 0; queryIndex < query.length; queryIndex++) {
+      recipePreview(queryIndex);
+    }
+  });
+  xhr.send();
+}
+
+function recipeRequest() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://api.spoonacular.com/recipes/' + recipeId + '/information?apiKey=a264ae5f3ca9445d94cfeb08761ae88b');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    recipe = xhr.response;
+  });
+  xhr.send();
+}
