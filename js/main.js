bb14b13f7af2463242b5a70f918eb6a9f559ffee
Irwin Chen <irwin.t.chen@gmail.com>
2022-04-19 18:50:24 +0000

    created dom tree, appends to page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var $form = document.querySelector('form');
 var $searchBar = document.querySelector('input');
+var $recipeContainer = document.querySelector('.recipe-container');
 var query;
 
 $form.addEventListener('submit', function (event) {
@@ -21,6 +22,34 @@ function serverRequest() {
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
     query = xhr.response;
+    for (var queryIndex = 0; queryIndex < query.length; queryIndex++) {
+      recipePreview(queryIndex);
+    }
   });
   xhr.send();
+}
+
+function recipePreview(index) {
+  var $recipePreviewEntry = document.createElement('div');
+  $recipePreviewEntry.className = 'column-third margin-bot-fifteen';
+
+  var $recipePreviewCard = document.createElement('div');
+  $recipePreviewCard.className = 'row flex-column align-center';
+
+  var $recipeImage = document.createElement('img');
+  $recipeImage.src = query[index].image;
+  $recipeImage.className = 'recipe-img';
+
+  var $recipeName = document.createElement('p');
+  $recipeName.textContent = query[index].title;
+  $recipeName.className = 'recipe-name';
+
+  var $recipeInfoButton = document.createElement('button');
+  $recipeInfoButton.className = 'info-button text-white';
+  $recipeInfoButton.textContent = 'READ MORE';
+  $recipeInfoButton.setAttribute('recipeId', query[index].id);
+
+  $recipePreviewCard.append($recipeImage, $recipeName, $recipeInfoButton);
+  $recipePreviewEntry.append($recipePreviewCard);
+  $recipeContainer.append($recipePreviewEntry);
 }
