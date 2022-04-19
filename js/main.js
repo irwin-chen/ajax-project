890c2474f732d280a9d985ccba4b1e0963d5d48f
Irwin Chen <irwin.t.chen@gmail.com>
2022-04-19 05:56:02 +0000

    saved search query as variable

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
 var $form = document.querySelector('form');
+var $searchBar = document.querySelector('input');
+var query;
 
 $form.addEventListener('submit', function (event) {
   event.preventDefault();
+  query = $searchBar.value;
+  parseQuery();
   $form.reset();
+  serverRequest();
 });
+
+function parseQuery() {
+  query = query.trim();
+  query = query.replaceAll(' ', ',+');
+}
+
+function serverRequest() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://api.spoonacular.com/recipes/findByIngredients' + '?ingredients=' + query + '&number=6&ranking=1&ignorePantry=true&apiKey=a264ae5f3ca9445d94cfeb08761ae88b');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    query = xhr.response;
+  });
+  xhr.send();
+}
